9e793d64984c0a1f3ebe8687e57b7342ff4fae38
MatisAgr <matisagr@gmail.com>
2025-01-30 17:19:46 +0100

    Renommer le champ 'sex' en 'gender' dans le modèle Match pour une terminologie plus inclusive

--- back/models/Match.js
+++ back/models/Match.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const MatchSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
     age: { type: String, required: true, unique: true },
-    sex: { type: String, required: true, enum: ['female', 'male', 'other', 'attack helicopter'] },
+    gender: { type: String, required: true},
     point: { type: Number, required: true },
     url_img: { type: String, require: true},
     createdAt: { type: Date, default: Date.now }
