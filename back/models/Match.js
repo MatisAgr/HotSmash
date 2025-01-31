67cbb8fc86739e48adc22b17cf744edd12a807a7
Tisma <matisagr@gmail.com>
2025-01-31 22:58:23 +0100

    Supprime l'unicité du champ 'age' dans le modèle Match

--- back/models/Match.js
+++ back/models/Match.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const MatchSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
-    age: { type: String, required: true, unique: true },
+    age: { type: String, required: true },
     gender: { type: String, required: true},
     point: { type: Number, required: true },
     url_img: { type: String, require: true},
