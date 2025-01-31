394d863394d9aad34fdd5326197209c109f3cb16
MatisAgr <matisagr@gmail.com>
2025-01-31 12:48:05 +0100

    Amélioration du formulaire de création de Smash : traduction des messages d'erreur, ajout d'un délai pour le message de succès, et validation des points. Ajustement de la mise en page pour une meilleure réactivité.

--- front/src/pages/CreateSmashPage/CreateSmashPage.jsx
+++ front/src/pages/CreateSmashPage/CreateSmashPage.jsx
@@ -1,15 +1,10 @@
-import React from 'react'
-
-import CreateSmashForm from '../../components/Form/CreateSmashForm'
+import React from 'react';
+import CreateSmashForm from '../../components/Form/CreateSmashForm';
 
 export default function CreateSmashPage() {
   return (
-    <>
-
-        <div className='flex flex-col items-center justify-center h-screen w-full bg-gradient-to-r from-purple-500 via-pink-500 to-red-500'>
-            <CreateSmashForm /> 
-        </div>
-        
-    </>
-  )
+    <div className="flex flex-col items-center justify-center min-h-screen w-full bg-gradient-to-r from-purple-500 via-pink-500 to-red-500">
+      <CreateSmashForm />
+    </div>
+  );
 }
