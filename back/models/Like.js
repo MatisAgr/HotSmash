094906fbd04d040583832f25899a511a38419a5b
MatisAgr <matisagr@gmail.com>
2025-01-31 09:56:51 +0100

    Ajout du champ createdAt au schéma Like pour enregistrer la date de création

--- back/models/Like.js
+++ back/models/Like.js
@@ -15,6 +15,10 @@ const likeSchema = new mongoose.Schema({
         type: Number,
         enum: [0, 1, 2],
         required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
     }
 }, {
     collection: 'likes',
